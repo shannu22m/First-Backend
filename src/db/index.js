06119803c526ff91017f00ef32322658c2f9bdf1
shannu22m <shanmukhamandapalli22@gmail.com>
2026-01-08 00:36:25 +0530

    Env configuration updated

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,5 @@
 import { DB_NAME } from "../constants.js";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config();
 const connectDB=async()=>{
     try {
         await mongoose.connect(process.env.MONGODB_URL,{
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import mongoose from 'mongoose';
 import { connectDB } from './db/index.js';
+import dotenv from 'dotenv';
 
+dotenv.config({
+    path:"./.env"
+})
 connectDB();
